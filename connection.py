47e7cc4963828b4e5addbb873f6c127fdecd3997
dschwarz <danielschwarz26@gmail.com>
2014-02-27 15:11:43 -0800

    Added get calls

--- connection.py
+++ connection.py
@@ -1,13 +1,15 @@
-import request
+import requests
 
 class Connection:
 	def __init__(self):
 		self.url_base = 'https://api.predictious.com/v1/'
-		api_key = 'rGTAtchx86jDxadDCOnKVQi8W8yKnnE708TIr1e9w/3Xs7M0mA6LKV0VgbtRN'
-		self.headers = {'X-Predictious-Key': self.api_key}		
+		api_key_2 = '2umz/dm6lXWqB1Mn9OAE8tr2wAI93iv3QE08M/p7GrTBW41U5yc5+AkEcxQjKJfF7pXyPzP8T/+9tm6/YOmA/jPwLXJab6p0oLWDTVKrbrnXRxYTXVcTxt9nsZzglhtoABG4VYWLpNp/ABBFftUHNAkdHI8='
+		self.headers = {'X-Predictious-Key': api_key_2}
 
-	def get(self, *kwargs):
-		endpoint = self.api_key + '/'.join(kwargs)
-		return request.get(endpoint, headers=self.headers)
-		
+	def get(self, *args):
+		endpoint = self.url_base + '/'.join(args)
+		return requests.get(endpoint, headers=self.headers, verify=False)
+	
+	def post(self, **kwargs):
+		pass	
 		
