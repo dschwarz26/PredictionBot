5aac4d7ec2d10dfc0a13ee3b8498824cb1dd471e
dschwarz <danielschwarz26@gmail.com>
2014-03-13 20:00:16 -0700

    Add email reporting

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import connection
 import probability
 import datetime
 import parse
+import smtplib
+
+from email.mime.text import MIMEText
 
 def get_total_funds(conn):
 	wallet_data = conn.retrieve_wallet()
@@ -34,14 +37,40 @@ def get_buy_and_sell_points(bids_and_asks):
 			lowest_ask = ask['Price']
 	return float(highest_bid) / 100000, float(lowest_ask) / 100000
 
+def send_email(option_data):
+	content = """\
+		<html>
+			<head></head>
+		  	<body>
+		"""
+	for option in option_data:
+		content += """\
+				<li>%s</li>
+		""" % option
+	content += """\
+			</body>
+		</html>
+		"""
+	print content
+	msg = MIMEText(content, 'html')
+	msg['Subject'] = 'PredictiousBot Data for %s' % datetime.datetime.utcnow().strftime('%Y-%m-%d')
+	msg['From'] = 'DanielSchwarz26@gmail.com'
+	msg['To'] = 'DanielSchwarz26@gmail.com'
+	s = smtplib.SMTP('localhost')
+	s.sendmail(msg['From'], msg['To'], msg.as_string())
+	s.quit()
+	
 if __name__ == '__main__':		
 	conn = connection.Connection()
 	rel_vol =  conn.get_bitcoin_volatility()
+	print 'Got volatility'
 	curr_price = float(conn.get_bitcoin_price())
+	print 'Got price'
 	vol = rel_vol * curr_price / 100
 	#print get_total_funds(conn)
 	#print get_available_funds(conn)
 	contracts = get_all_bitcoin_contracts(conn)
+	option_data = []
 	for key, val in contracts.iteritems():
 		name_data = parse.parse_contract_name(key)
 		if name_data:
@@ -52,7 +81,10 @@ if __name__ == '__main__':
 				vol
 			)
 			highest_bid, lowest_ask = get_buy_and_sell_points(val)
-			print('Contract %s has value %.3f. Highest bid: %.2f. Lowest ask: %.2f' % (
+			option_data.append('Contract %s has value %.3f. Highest bid: %.2f. Lowest ask: %.2f' % (
 				key, option_value, highest_bid, lowest_ask))
+	print 'Got option data'
+	send_email(option_data)
+	print 'Sent email'
 	#print conn.retrieve_recent_transactions()
 	#print conn.retrieve_current_contracts()
